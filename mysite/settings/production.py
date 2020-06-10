d918460f9dca13a2eabca6da1095d8e6a7537938
zainabade <zainabadelopo@gmail.com>
2020-06-10 19:08:41 +0100

    Added Whitenoise

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -19,3 +19,11 @@ try:
    from .local import *
 except ImportError:
    pass
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
